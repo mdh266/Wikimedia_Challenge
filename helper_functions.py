f50637f6edc732ee2bd1458b2c1c5f892a0d333b
Mike <mdh266@gmail.com>
2017-03-14 15:15:18 -0400

    first draft.

---
+++ helper_functions.py
@@ -0,0 +1,165 @@
+"""
+These are some helper functions to perform Pandas tricks.
+They were included here instead of the jupyter notebook
+to keep the noteboo shorter.
+"""
+
+import pandas as pd
+import numpy as np
+import pandas as pd
+
+def searched_per_day_by_group(df):
+    """
+    Returns the number of searches per day in group 'a' 
+    and group 'b'
+    
+    :param: df (Pandas DataFrame):  DataFrame indexed by pandas.DateTime
+    
+    :returns: Series with the day and the number of searches
+              in that day for 'group a' and 'group b'
+    
+    :rvalue: Pandas Series
+    """
+    return df.groupby([pd.TimeGrouper('D'),
+                       'group']).apply(lambda row:
+                                len(row[row['action'] == 'searchResultPage']))
+
+
+def searched_per_day(df):
+    """
+    Returns the number of searches per day regardless of group
+    
+    :param: df (Pandas DataFrame):  DataFrame indexed by pandas.DateTime
+    
+    :returns: Series with the day and the percentage of searches in that day
+    
+    :rvalue: Pandas Series
+    """
+    return df.groupby([pd.TimeGrouper('D')]).apply(lambda row: 
+                                  len(row[row['action'] == 'searchResultPage']))
+
+
+def average_click_through_rate(df):
+    """
+    Returns the average number of clickthroughs per day
+    
+    :param: df (Pandas DataFrame):  DataFrame indexed by pandas.DateTime
+    
+    :returns: Series with the day and the percentage of clickthroughs 
+              in that day 
+    
+    :rvalue: float
+    """
+    # click through by day
+    CT_PerDay = df.groupby([pd.TimeGrouper('D')]).apply(lambda row: 
+                                  len(row[row['action'] == 'visitPage']))
+    
+    Searches_PerDay = searched_per_day(df)
+    
+    Daily_Averages = CT_PerDay / Searches_PerDay
+    
+    return Daily_Averages.mean()
+
+
+def average_click_through_rate_by_group(df):
+    """
+    Returns the average number of clickthroughs per day by group 'a' vs. 'b'
+    
+    :param: d: Pandas DataFrame indexed by pandas.DateTime
+    
+    :returns: Series with the day and the percentage of clickthroughs 
+              in that day for 'group a' and 'group b'
+    
+    :rvalue: Pandas Series
+    """
+    Searches_PerDay_By_Group = searched_per_day_by_group(df)
+    CT_PerDay_By_Group = df.groupby([pd.TimeGrouper('D'),
+                                     'group']).apply(lambda row: 
+                                               len(row[row['action'] == 'visitPage']))
+
+    
+    Daily_Averages_By_Group = CT_PerDay_By_Group / Searches_PerDay_By_Group
+
+    # groupby the second entry in the multi-tuple index
+    return Daily_Averages_By_Group.groupby(level=[1]).mean()
+
+    
+def average_zero_rate(df):
+    """
+    Returns the average daily number of searches that return zero results.
+    
+    :param: df: Pandas dataframe indexed by pandas.DateTime
+    
+    :returns: average daily percentage of searches that return zero results.
+    
+    :rvalue: float
+    """
+    Searches_PerDay = searched_per_day(df)
+    
+    zero_results = df.groupby([pd.TimeGrouper('D')]).apply(lambda row: 
+                                                   len(row[row['n_results'] == 0]))
+    
+    perecent_zeros = zero_results / Searches_PerDay
+    
+    return perecent_zeros.mean()
+
+
+def average_zero_rate_by_group(df):
+    """
+    Returns the average daily number of searches that have zero 
+    results by group.
+    
+    :param: df: Pandas dataframe indexed by pandas.DateTime
+    
+    :returns: Series with the day and the percentage of searches 
+              that return zero results
+              in that day for 'group a' and 'group b'
+              
+    :rvalue: Pandas Series
+    """
+    Searches_PerDay_By_Group = searched_per_day_by_group(df)
+    
+    zero_results = df.groupby([pd.TimeGrouper('D')]).apply(lambda row: 
+                                                   len(row[row['n_results'] == 0]))
+    
+    perecent_zeros = zero_results / Searches_PerDay_By_Group
+    
+    # groupby the second entry in the multi-tuple index
+    return perecent_zeros.groupby(level=[1]).mean()
+
+
+def get_sessions_with_n_results(df, nresults):
+    """
+    Returns a Pandas Series where values
+    are percentages of sessions with n_results = nresults 
+    and session_length in different session length intervals.
+    
+    :param: df (Pandas DataFrame) : The merged dataframe (df3)
+    
+    :param:  nresults (int) : The number of listed results from the
+              search.
+    
+    :returns: Series of the percentage of sessions with 
+              n_results = nresults with session times in different
+              intervals.
+              
+    :rvalue: Pandas Series
+    """
+    bins = [0,10,30,60,90,120,150,180,210,240]
+    time = 240
+    for i in range(6):
+        time += 60
+        bins.append(time)
+        
+    results = df[df.n_results_end == nresults]
+    num_sessions = results.shape[0]
+
+    result_session_length = results['time_end'] - results['time_start']
+    result_session_length = \
+                    result_session_length.apply(lambda row: row.seconds)
+    result_top_session_length = \
+                    result_session_length.value_counts(bins=bins)
+
+    result_top_session_length /= num_sessions
+    
+    return result_top_session_length
---
+++ plotting_functions.py
@@ -0,0 +1,130 @@
+"""
+This contains some functions to make plots that were a little
+move involved. They are in this file to save space in the 
+juptyer notebook.
+"""
+
+import pandas as pd
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+    
+def plot_grouped_and_ungrouped(diff, rate, string):
+    """
+    Function that plots the Pandas Series values as bar graph. 
+    
+    :param: rate (float) : The value of the ungrouped results to 
+                          be plotted.
+                          
+    :param: diff (Pandas Series) : The Series is usually the grouped 
+                                   results.
+
+    :param: string (str) : The description of the value to be plotted.
+    """
+    plt.figure(figsize=(4, 4))
+    diff = diff.append(pd.Series([rate], index=['ungrouped']))
+    diff.plot(kind='bar')
+    plt.title(string)
+    plt.xticks(rotation=0)
+    plt.show()
+    
+    
+def plot_daily_clicked(first_clicked, top_num):
+    """
+    Plots bar graphs of the daily number of clicks on the each search
+    result by order of the listing.  The x-axis will be the ordering
+    of the webpage search result and y-axis is the number of people's 
+    whose first click was that result_position.  Each bar graph
+    represents one of the eight days. 
+    
+    :param: first_clicked (Pandas Series) : This multi-index series.
+            index level 0 - date
+            index level 1 - session_id
+            values - result_position
+            
+    :param: top_num (int): The number of web page clicks.
+    """
+    days = ['2016-03-01', '2016-03-02', '2016-03-03', '2016-03-04',
+            '2016-03-05', '2016-03-06', '2016-03-07', '2016-03-08']
+
+    day = 0
+    string = ""
+    plt.figure(figsize=(6, 7))
+    for j in range(1,9):
+        plt.subplot(4,2,j)
+        # count the top 20 highest clicked links of this day
+        day_clicked =\
+            first_clicked[days[day]].value_counts().sort_index().head(top_num)
+      
+        plt.bar(day_clicked.index, day_clicked.values)
+        plt.ylabel('Count')
+        plt.title(days[day],fontsize=10)
+        plt.xlabel('Result Position')
+        plt.ylim([0,2500])
+        day += 1
+    
+    plt.tight_layout()
+    plt.show()
+
+    
+def plot_session_length_by_group(session_length_by_group):
+    """
+    Plots the frquency of sessions in group a and group b
+    that have a session_length with in certain time inverals
+    that were determined beforehand.
+    
+    :param: session_length_by_group (Pandas Series) : The frequency
+                                        of session length by group.
+    """
+    plt.figure(figsize=(6,4))
+    fig, ax = plt.subplots()
+    rects1 = session_length_by_group['a'].sort_index().plot(kind='bar',
+                                                        color='b',
+                                                        label='Group a')
+    
+    rects2 = session_length_by_group['b'].sort_index().plot(kind='bar',
+                                                        color='r',
+                                                        label='Group b')
+    plt.ylabel('Count Of Sessions')
+    plt.xlabel('Session Length In Seconds')
+    plt.title('Session Length Between The Groups')
+    plt.legend()
+    plt.show()
+    
+
+def plot_session_length_by_group_percentages(session_length_a,
+                                             session_length_b):
+    """
+    Plots the percentages of sessions in group a and group b
+    that had a session_length with in certain time inverals
+    that were determined beforehand.
+    
+    
+    :param: sesssion_length_a (Pandas Series) : 
+    :param: sesssion_length_b(Pandas Series) : 
+    """
+    
+    plt.figure(figsize=(9,5))
+    plt.subplot(121)
+    session_length_a.sort_index().plot(kind='bar',
+                                    color='b',
+                                    label='Group a')
+    
+    plt.title('Percentage Of Sessions By Session Length',
+            fontsize=10)
+
+    plt.ylim([0,0.25])
+    plt.legend()
+    plt.subplot(122)
+
+    rects2 = session_length_b.sort_index().plot(kind='bar',
+                                            color='r',
+                                            label='Group b')
+    
+    plt.title('Percentage Of Sessions By Session Length',
+            fontsize=10)
+
+    plt.ylim([0,0.25])
+    plt.legend()
+    plt.show()
